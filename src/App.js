b35487de43a02fee04c2e3c5c89c8f1c57cad8ad
sohail shahzad <sohail.shahzad54@gmail.com>
2020-09-27 19:33:40 +0500

    The update

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dinner from './dinner.js';
+
+function App() {
+  //const name = "<h1>Hello World in App.js</h1>"
+  return (
+      <div className="App">
+          <Dinner/>
+      </div>
+  );
+  
+}
+
+export default App;
---
+++ src/dinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Dinner() {
+
+    return (
+        <div>Return From Dinner.js File</div>
+            )
+    }
+
+export default Dinner; 
